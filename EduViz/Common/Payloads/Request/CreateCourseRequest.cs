160e748013050acbac209a81b16d4c044a9743f6
My Name <nguyenvuquanghuy11@gmail.com>
2024-09-28 22:42:37 +0700

    update create course request

--- EduViz/Common/Payloads/Request/CreateCourseRequest.cs
+++ EduViz/Common/Payloads/Request/CreateCourseRequest.cs
@@ -16,7 +16,7 @@ public class CreateCourseRequest
     
     public string startDate { get; set; }
     public string monthDuration { get; set; }
-    public IFormFile picture { get; set; }
+    public IFormFile? picture { get; set; }
     public string beginingClass { get; set; }
     public string endingClass { get; set; }
 }
@@ -25,6 +25,10 @@ public static class CreateCourseRequestExtensions
 {
     public static CourseModel ToCourseModel(this CreateCourseRequest courseRequest)
     {
+        if (courseRequest.picture is null)
+        {
+            throw new BadRequestException("Picture can not be null");
+        }
 
         int price;
         if (!(int.TryParse(courseRequest.price, out price)))
